950d4e62d5c4dbf8760b2087ed63258e096b3d9d
Samba <rocklee.no21@gmail.com>
2019-06-01 19:15:56 +0530

    User validation with Redux added

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,22 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
+
 import './App.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path="/" component={Landing} />
       <section className="container">
+        <Alert />
         <Switch>
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
@@ -20,6 +28,7 @@ const App = () => (
       </section>
     </Fragment>
   </Router>
+  </Provider>
 );
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,15 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 // import axios from 'axios';
+import { connect } from 'react-redux';
 
-const Register = () => {
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth'; 
+
+import PropTypes from 'prop-types'
+
+
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -17,9 +24,9 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if(password !== password2) {
-            console.log('Passwords do not match')
+            setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('SUCCESS');
+            register({ name, email, password });
             // const newUser = {
             //     name,
             //     email,
@@ -48,7 +55,7 @@ const Register = () => {
                 <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
                 <form className="form" onSubmit={e => onSubmit(e)} >
                     <div className="form-group">
-                        <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} required />
+                        <input type="text" placeholder="Name" name="name" value={name} onChange={e => onChange(e)} />
                     </div>
                     <div className="form-group">
                         <input type="email" placeholder="Email Address" name="email" value={email} onChange={e => onChange(e)} />
@@ -62,7 +69,6 @@ const Register = () => {
                             type="password"
                             placeholder="Password"
                             name="password"
-                            minLength="6"
                             value={password}
                             onChange={e => onChange(e)}
                             />
@@ -72,7 +78,6 @@ const Register = () => {
                             type="password"
                             placeholder="Confirm Password"
                             name="password2"
-                            minLength="6"
                             value={password2}
                             onChange={e => onChange(e)}
                             />
@@ -86,4 +91,9 @@ const Register = () => {
     )
 }
 
-export default Register;
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
+}
+
+export default connect(null, { setAlert, register })(Register);
