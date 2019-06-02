691b1cc815a4b7e089287a9430526110d281847a
Samba <rocklee.no21@gmail.com>
2019-06-02 16:18:35 +0530

    Protected routes and simple dashboard added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,35 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
 
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/route/PrivateRoute';
 
-import './App.css';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
 
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
-const App = () => (
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
   <Provider store={store}>
   <Router>
     <Fragment>
@@ -24,11 +40,13 @@ const App = () => (
         <Switch>
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
       </section>
     </Fragment>
   </Router>
   </Provider>
-);
+  )
+};
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 // import axios from 'axios';
 import { connect } from 'react-redux';
 
@@ -9,7 +9,7 @@ import { register } from '../../actions/auth';
 import PropTypes from 'prop-types'
 
 
-const Register = ({ setAlert, register }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -27,26 +27,11 @@ const Register = ({ setAlert, register }) => {
             setAlert('Passwords do not match', 'danger');
         } else {
             register({ name, email, password });
-            // const newUser = {
-            //     name,
-            //     email,
-            //     password
-            // }
-
-            // try {
-            //     const config = {
-            //         headers: {
-            //             'Content-Type': 'application/json'
-            //         }
-            //     }
-
-            //     const body = JSON.stringify(newUser);
-            //     const res = await axios.post('/api/users', body, config);
-            //     console.log(res.data);
-            // } catch (err) {
-            //     console.error(err.response.data);
-            // }
         }
+    }
+
+    if(isAuthenticated) {
+       return <Redirect to="/dashboard" />
     }
 
     return (
@@ -93,7 +78,13 @@ const Register = ({ setAlert, register }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
-    register: PropTypes.func.isRequired
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
 }
 
-export default connect(null, { setAlert, register })(Register);
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
