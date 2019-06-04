145f2bf256cf614474e806569d78d23ed0aa76b8
Samba <rocklee.no21@gmail.com>
2019-06-04 16:35:54 +0530

    Experience and education field added in the developer views link

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -30,7 +30,7 @@ export const getCurrentProfile = () => async dispatch => {
 
 
 // Get all profiles
-export const getProfile = () => async dispatch => {
+export const getProfiles = () => async dispatch => {
     dispatch({ type: CLEAR_PROFILE });
 
     try {
@@ -53,7 +53,7 @@ export const getProfileById = userId => async dispatch => {
     try {
         const res = await axios.get(`/api/profile/user/${userId}`);
         dispatch({
-            type: GET_PROFILES,
+            type: GET_PROFILE,
             payload: res.data
         });
     } catch (err) {
@@ -231,7 +231,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
     if(window.confirm('Are you sure? This can NOT be undone!')) {
         try {
-            const res = await axios.delete('/api/profile');
+            await axios.delete('/api/profile');
     
             dispatch({type: CLEAR_PROFILE});
             dispatch({type: ACCOUNT_DELETED});
