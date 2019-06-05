31eecb98ca526a11a60e50f69cf0c2771c8b820b
Samba <rocklee.no21@gmail.com>
2019-06-05 08:15:09 +0530

    Comment add and remove functionality added.

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
 
@@ -14,6 +16,10 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
     return loading || post === null ? <Spinner /> : <Fragment>
         <Link to="/posts" className='btn'>Back To Posts</Link>
         <PostItem post={post} showActions={false} />
+        <CommentForm postId={post._id} />
+        {post.comments.map(comment => (
+            <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
     </Fragment>
 }
 
